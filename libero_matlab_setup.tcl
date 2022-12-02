821c0cc9f4706adccb0eabc14522c081d795c5f0
Eric <eraguzin@bnl.gov>
2022-12-02 17:53:14 -0500

    Big update to take arguments and take on more cases of setting up projects

--- libero_matlab_setup.tcl
+++ libero_matlab_setup.tcl
@@ -2,67 +2,100 @@
 # libero
 # Date: Wed Nov 23 15:33:55 2022
 # Will start up libero with the exported Matlab files
-puts "Welcome to Eric's Matlab TCL script"
-set folder_name [lindex $argv 0]
-puts "Folder name is $folder_name"
-set location "/home/eraguzin/nextcloud/LuSEE/matlab_transfer/$folder_name/libero_proj"
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Get all directory values
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+puts "Welcome to Eric's Matlab Setup TCL script"
+set directory_path [lindex $argv 0]
+set module [lindex $argv 1]
+puts "Directory Path is $directory_path"
+set project_name "libero_proj_$module"
+set location "$directory_path/$project_name"
 puts "Location is $location"
-
-puts "Number is $argc"
-
-set file_name [lindex $argv 1]
-puts "File name is $file_name"
-set file1 "/home/eraguzin/nextcloud/LuSEE/matlab_transfer/$folder_name/codegen/$file_name/hdlsrc/$file_name\_fixpt.vhd"
-puts "File location is $file1"
-set file2 "/home/eraguzin/nextcloud/LuSEE/matlab_transfer/$folder_name/codegen/$file_name/hdlsrc/$file_name\_fixpt_pkg.vhd"
-puts "File location is $file2"
-set file3 "/home/eraguzin/nextcloud/LuSEE/matlab_transfer/$folder_name/codegen/$file_name/hdlsrc/$file_name\_fixpt_tb.vhd"
-puts "File location is $file3"
-set file4 "/home/eraguzin/nextcloud/LuSEE/matlab_transfer/$folder_name/codegen/$file_name/hdlsrc/$file_name\_fixpt_tb_pkg.vhd"
-puts "File location is $file4"
-set root "$file_name\_fixpt"
+set hdl_location "$directory_path/codegen/$module/hdlsrc/"
+puts "File name is $module"
+set root "$module\_fixpt"
 puts "Root is $root"
-set file5 "/home/eraguzin/nextcloud/LuSEE/matlab_transfer/$folder_name/libero_proj/stimulus/$file_name\_fixpt_tb.vhd"
-puts "File location is $file5"
-set file6 "/home/eraguzin/nextcloud/LuSEE/matlab_transfer/$folder_name/libero_proj/stimulus/$file_name\_fixpt_tb_pkg.vhd"
-puts "File location is $file6"
-set tb_name "$file_name\_fixpt_tb"
+set tb_name "$module\_fixpt_tb"
 puts "Testbench name is $tb_name"
 
-new_project -location $location -name {libero_proj} -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -use_relative_path 0 -linked_files_root_dir_env {} -hdl {VHDL} -family {PolarFire} -die {MPF300T} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {EXT} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} -adv_options {TEMPR:EXT} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:EXT}
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Get all files with certain extensions
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Get all files with .dat extension. Needed for simulation
+set dat_files [list]
+lappend dat_files {*}[glob -dir $hdl_location *.dat]
+#puts "dat_files are $dat_files"
+
+#Get all files with _tb text. Needed for simulation
+set tb_pkg_file [glob -dir $hdl_location *_tb_pkg.vhd]
+set tb_file [glob -dir $hdl_location *_tb.vhd]
+#puts "tb_files are $tb_file"
+#puts "tb_pkg files are $tb_pkg_file"
+
+#Get all VHDL files
+set vhdl_files [list]
+lappend vhdl_files {*}[glob -dir $hdl_location *.vhd]
+
+#Remove the test bench files from the list of VHDL files
+set idx [lsearch $vhdl_files $tb_pkg_file]
+set vhdl_files [lreplace $vhdl_files $idx $idx]
+
+set idx [lsearch $vhdl_files $tb_file]
+set vhdl_files [lreplace $vhdl_files $idx $idx]
+
+set tb_pkg_project_file "$location/stimulus/$module\_fixpt_tb.vhd"
+puts "File location is $tb_pkg_project_file"
+set tb_project_file "$location/stimulus/$module\_fixpt_tb_pkg.vhd"
+puts "File location is $tb_project_file"
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Libero operations, create the project
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+new_project -location $location -name $project_name -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -use_relative_path 0 -linked_files_root_dir_env {} -hdl {VHDL} -family {PolarFire} -die {MPF300T} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {EXT} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} -adv_options {TEMPR:EXT} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:EXT}
 set_device -family {PolarFire} -die {MPF300T} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {EXT} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} -adv_options {TEMPR:EXT} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:EXT} 
-import_files \
+
+#Add each VHDL file
+foreach vhdl_file $vhdl_files {
+    import_files \
          -convert_EDN_to_HDL 0 \
          -library {work} \
-         -hdl_source $file1
-
-if {[file exists file2] == 1} {
-    import_files \
-        -convert_EDN_to_HDL 0 \
-        -library {work} \
-        -hdl_source $file2
-    puts "$file2 was copied!"
-} else {
-    puts "$file2 doesn't exist!"
+         -hdl_source $vhdl_file
 }
 
+#Add simulation files as stimulus
 import_files \
          -convert_EDN_to_HDL 0 \
          -library {work} \
-         -stimulus $file3
+         -stimulus $tb_pkg_file
 
 import_files \
          -convert_EDN_to_HDL 0 \
          -library {work} \
-         -stimulus $file4
+         -stimulus $tb_file
 
+#Necessary because Libero is in project mode. Equivalent to pressing "Build Heirarchy" button in GUI and selecting top level
 build_design_hierarchy
 set_root -module $root\::work
-organize_tool_files -tool {SIM_PRESYNTH} -file $file6 -file $file5 -module $root\::work -input_type {stimulus}
-organize_tool_files -tool {SIM_POSTSYNTH} -file $file6 -file $file5 -module $root\::work -input_type {stimulus}
-organize_tool_files -tool {SIM_POSTLAYOUT} -file $file6 -file $file5 -module $root\::work -input_type {stimulus}
+
+#pkg files must come first or else ModelSim doesn't know where to get the library specified in the test bench file
+organize_tool_files -tool {SIM_PRESYNTH} -file $tb_pkg_project_file -file $tb_project_file -module $root\::work -input_type {stimulus}
+organize_tool_files -tool {SIM_POSTSYNTH} -file $tb_pkg_project_file -file $tb_project_file -module $root\::work -input_type {stimulus}
+organize_tool_files -tool {SIM_POSTLAYOUT} -file $tb_pkg_project_file -file $tb_project_file -module $root\::work -input_type {stimulus}
+
+#Equivalent of going to Project Options and changing DO file area. Testbench will have a different name then default, so it needs to know
 set_modelsim_options -tb_module_name $tb_name
 save_project
-#run_tool -name {SIM_PRESYNTH}
+
+foreach dat_file $dat_files {
+    file copy $dat_file "$location/simulation"
+}
+
+#Run specific tools
+run_tool -name {SIM_PRESYNTH}
 #run_tool -name {SYNTHESIZE}
 #save_project
--- matlab_hdl_transfer.sh
+++ matlab_hdl_transfer.sh
@@ -1,32 +1,228 @@
 #! /usr/bin/bash
-echo "Hello World"
 
-if [ $# -ne 2 ]
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Parse the input arguments
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Example values
+#scp_source=eraguzin@silicon.inst.bnl.gov:/u/home/eraguzin/matlab/
+#local_project=~/nextcloud/LuSEE/matlab_transfer
+#directory=matlab
+#file=sfft
+#create=true
+#move=true
+
+#Options parser
+#https://unix.stackexchange.com/questions/321126/dash-arguments-to-shell-scripts
+
+#If you need to tell the user how to use the command
+PROGNAME=$0
+usage() {
+  cat << EOF >&2
+Usage: $PROGNAME [-v] [-d <dir>] [-f <file>]
+
+-f <file>: ...
+ -d <dir>: ...
+       -v: ...
+EOF
+  exit 1
+}
+
+#Checks that there is or isn't an argument as appropriate
+check_for_argument() {
+    if [ $1 == true ];
     then
-        echo "Script requires 2 argument, closing"
-        exit 0
+        if [ "${2::1}" == "-" ];
+        then
+            echo "$3 command requires an argument!"
+            usage
+        fi
+    else
+
+        #If it's the last one then $3 will be empty. If it isn't, the next "argument" should be the next command
+        if [ "${2::1}" != "-" ] && [ ! -z "$3" ];
+        then
+            echo "$3 command does not take any arguments!"
+            usage
+        fi
+    fi
+}
+
+#Parse the arguments
+scp_source=none
+local_project=none
+directory=none
+file=none
+create=false
+move=false
+
+#Parse with long arguments
+#https://stackoverflow.com/a/7680682
+
+while getopts :-:s:l:d:f:cm o; do
+  case $o in
+    -)
+            case "${OPTARG}" in
+                scp_source)
+                    #Has to manually get the next indexed argument
+                    check_for_argument true ${!OPTIND} "scp_source"
+                    scp_source="${!OPTIND}"
+                    echo "SCP Source will be" $scp_source
+                    #Need to manually shift after getting argument because of hacky way to have two dash arguments
+                    shift 1
+                    ;;
+
+                local_project)
+                    check_for_argument true ${!OPTIND} "local_project"
+                    local_project="${!OPTIND}"
+                    echo "Local Project location will be" $local_project
+                    shift 1
+                    ;;
+
+                directory)
+                    check_for_argument true ${!OPTIND} "directory"
+                    directory="${!OPTIND}"
+                    echo "Directory location will be" $directory
+                    shift 1
+                    ;;
+
+                file) file="${!OPTIND}"
+                    check_for_argument true ${!OPTIND} "file"
+                    file="${!OPTIND}"
+                    echo "Main Libero file name will be" $file
+                    shift 1
+                    ;;
+
+                create)
+                    check_for_argument false ${!OPTIND} "create"
+                    echo "Will create libero project"
+                    create=true
+                    ;;
+
+                move)
+                    check_for_argument false ${!OPTIND} "move"
+                    echo "Will SCP Matlab project"
+                    move=true
+                    ;;
+                *) usage
+            esac;;
+
+    s)
+        check_for_argument true $OPTARG "scp_source"
+        scp_source=$OPTARG
+        echo "SCP Source will be" $scp_source
+        ;;
+
+    l)
+        check_for_argument true $OPTARG "local_project"
+        local_project=$OPTARG
+        echo "Local Project will be" $local_project
+        ;;
+
+    d)
+        check_for_argument true $OPTARG "directory"
+        directory=$OPTARG
+        echo "Directory location will be" $directory
+        ;;
+
+    f)
+        check_for_argument true $OPTARG "file"
+        file=$OPTARG
+        echo "Main Libero file name will be" $file
+        ;;
+
+    c)
+        echo "Will create libero project"
+        create=true
+        ;;
+
+    m)
+        echo "Will SCP Matlab project"
+        move=true
+        ;;
+
+    *) usage
+  esac
+done
+echo "SCP source is" $scp_source
+echo "Local project is" $local_project
+echo "Directory location is" $directory
+echo "File is" $file
+echo "Create is" $create
+echo "Move is" $move
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Do the scp if necessary
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+if [ $move == true ]
+then
+    if  [ $scp_source != none ] && [ $local_project != none ]
+    then
+        echo "-------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+        echo "SCP INITIALIZING
+        scp -r $scp_source $local_project
+        echo "SCP FINISHED
+        echo "-------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    else
+        echo "You need to specify a source and destination for the SCP to move files over"
+    fi
 fi
 
-echo "Entered arg is $@"
+if [ $create == false ]
+then
+    echo "Finished"
+    exit 0
+else
+    if [ $local_project == none ] || [ $file == none ];
+    then
+        echo "You need to specify a local project directory and file name"
+        exit 0
+    fi
+fi
 
-for x in $@
-do
-    echo "Entered arg is $@"
-done
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Start the Libero project, run that TCL file
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+export LM_LICENSE_FILE=1702@iolicense2.inst.bnl.gov:7180@iolicense2.inst.bnl.gov:7184@iolicense2.inst.bnl.gov
 
-scp -r eraguzin@silicon.inst.bnl.gov:/u/home/eraguzin/matlab/$1 ~/nextcloud/LuSEE/matlab_transfer
-#mkdir -p ~/nextcloud/LuSEE/matlab_transfer/$1/libero_proj
-directory_location=$1
-dir_loc_trunc=${directory_location#*/}
-echo "Truncated directory location is $dir_loc_trunc"
-/usr/local/microchip/Libero_SoC_v2022.2/Libero/bin64/libero script:libero_matlab_setup.tcl "script_args:$dir_loc_trunc $2" logfile:make_libero.log
+if [ $scp_source != none ]
+then
+    #https://unix.stackexchange.com/questions/247560/print-everything-after-a-slash
+    #https://stackoverflow.com/questions/4651437/how-do-i-set-a-variable-to-the-output-of-a-command-in-bash
+    directory=$(echo $scp_source | sed 's:.*/::')
+    echo "Directory is now" $directory
+else
+    if [ $directory == none ]
+    then
+        echo "You need to specify a specific directory for the Matlab project"
+        exit 0
+    fi
+fi
 
-data_files=$(find ~/nextcloud/LuSEE/matlab_transfer/$dir_loc_trunc/codegen/$2/hdlsrc -type f -name "*.dat")
+location_to_send=$local_project/$directory
+
+/usr/local/microchip/Libero_SoC_v2022.2/Libero/bin64/libero script:libero_matlab_setup.tcl "script_args:$location_to_send $file" logfile:make_libero.log
+
+exit 0
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Move relevant files around for test bench
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+data_files=$(find $local_project/codegen/$file/hdlsrc -type f -name "*.dat")
 for file in $data_files
     do
         echo "Moving $file"
-        mv $file ~/nextcloud/LuSEE/matlab_transfer/$dir_loc_trunc/libero_proj/simulation
+        mv $file $local_project/libero_proj/simulation
     done
 
-export LM_LICENSE_FILE=1702@iolicense2.inst.bnl.gov:7180@iolicense2.inst.bnl.gov:7184@iolicense2.inst.bnl.gov
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Simulate and edit the DO file
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Start Libero
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 /usr/local/microchip/Libero_SoC_v2022.2/Libero/bin/libero
